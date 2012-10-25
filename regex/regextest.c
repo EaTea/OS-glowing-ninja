c624055f48bd837547330c8b9a7d66ca885429ae
Nic Barbaro <nictheman@gmail.com>
2012-10-25 14:18:45 +0800

    regex now finds submatches.

--- regex/regextest.c
+++ regex/regextest.c
@@ -13,22 +13,32 @@ const char *PATTERN =
 int main() {
 	regex_t regex;
 	int reti;
+	
 	char msgbuf[100];
-
+	int nmatches = 1024;
+	regmatch_t m[1024];
+	char *line = "if  I < 5 i=i+1 goto 100";
 /* Compile regular expression */
 	reti = regcomp(&regex, PATTERN, REG_ICASE);
 	if( reti ){ fprintf(stderr, "Could not compile regex\n"); exit(1); }
 
 /* Execute regular expression */
-	reti = regexec(&regex, "if  I < 5 i=i+1 goto 100", 0, NULL, 0);
+	reti = regexec(&regex, line, nmatches, m, 0);
 	if( !reti ){
-		puts("Match");
-		
+		puts("MATCHO");
+		//Ifline found
+		int s = (int)m[2].rm_so, e = (int)m[2].rm_eo;
+		printf("%d %d\n",s,e);
+		for (int i = s; i <= e; i++) {
+			printf("%c",line[i]);
+		} printf("\n");
 	}
 	else if( reti == REG_NOMATCH ){
-		puts("No match");
+		//Not an ifline -- Normal!
+		puts("NOMATCHO");
 	}
 	else{
+		//Aww balls. Somethign went horrid.
 		regerror(reti, &regex, msgbuf, sizeof(msgbuf));
 		fprintf(stderr, "Regex match failed: %s\n", msgbuf);
 		exit(1);
